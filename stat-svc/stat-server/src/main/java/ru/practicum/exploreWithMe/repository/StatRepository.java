2a2ba0fd78c21bcd6c9b1810f8f5693b41cff15e
Arina <isebialublu@yandex.ru>
2024-10-05 20:34:20 +0300

    fix: api tests

--- stat-svc/stat-server/src/main/java/ru/practicum/exploreWithMe/repository/StatRepository.java
+++ stat-svc/stat-server/src/main/java/ru/practicum/exploreWithMe/repository/StatRepository.java
@@ -27,13 +27,13 @@ public interface StatRepository extends JpaRepository<EndpointHits, Long> {
             "FROM EndpointHits e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
             "AND (:uris IS NULL OR e.uri IN :uris) " +
-            "GROUP BY e.app, e.uri")
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e) DESC")
     List<Statistics> getStatisticsForPeriodAndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.exploreWithMe.model.Statistics(e.app, e.uri, COUNT(e)) " +
             "FROM EndpointHits e " +
             "WHERE e.timestamp BETWEEN :start AND :end " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER BY COUNT(e) DESC")
+            "GROUP BY e.app, e.uri")
     List<Statistics> getStatisticsForPeriod(LocalDateTime start, LocalDateTime end);
 }
