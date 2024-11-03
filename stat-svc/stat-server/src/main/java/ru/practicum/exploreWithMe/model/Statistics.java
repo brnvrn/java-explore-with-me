b6a8c903db9b2225badf5a9cc79b420b9ca59a4d
Arina <isebialublu@yandex.ru>
2024-11-03 16:29:20 +0300

    fix: pt

--- stat-svc/stat-server/src/main/java/ru/practicum/exploreWithMe/model/Statistics.java
+++ stat-svc/stat-server/src/main/java/ru/practicum/exploreWithMe/model/Statistics.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practicum.exploreWithMe.dto.StatisticsDto;
-
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -16,17 +13,4 @@ public class Statistics {
     private String app;
     private String uri;
     private Long hits;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StatisticsDto that = (StatisticsDto) o;
-        return Objects.equals(hits, that.getHits());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(hits);
-    }
 }
